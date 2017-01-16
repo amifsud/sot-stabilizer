cc9171d2f613ff97e6741a3e67afd18bd5ccd5c9
Alexis MIFSUD <alexis.mifsud@gmail.com>
2017-01-16 17:57:29 +0100

    adding system initialization in the test of the smc stabilizer

--- unit-testing/test_hrp2-smc-stabilizer.cpp
+++ unit-testing/test_hrp2-smc-stabilizer.cpp
@@ -1,7 +1,9 @@
 #include <sot-stabilizer/tools/hrp2.hpp>
 #include <sot-state-observation/tools/definitions.hh>
-#include <sot-stabilizer/hrp2-smc-stabilizer.hh>
 #include <state-observation/tools/miscellaneous-algorithms.hpp>
+
+#include <sot-stabilizer/hrp2-smc-stabilizer.hh>
+#include <sot-stabilizer/prototyping/non-linear-rotational-table-cart-device.hh>
 
 using namespace sotStabilizer;
 using namespace sotStateObservation;
@@ -20,15 +22,41 @@ int testModel()
     const unsigned stateSize=6;
     const unsigned controlSize=3;
 
-    // Input initialization
+    /// System initialization
+    NonLinearRotationalTableCartDevice system("System");
+    system.setRobotMass(56.8);
+    system.setContactsNumber(2);
+    system.setKfe(convertMatrix<dynamicgraph::Matrix>(10000*Matrix3::Identity()));
+    system.setKfv(convertMatrix<dynamicgraph::Matrix>(600*Matrix3::Identity()));
+    system.setKte(convertMatrix<dynamicgraph::Matrix>(600*Matrix3::Identity()));
+    system.setKtv(convertMatrix<dynamicgraph::Matrix>(60*Matrix3::Identity()));
+    stateObservation::Matrix3 I;
+    I << 48.0,0,0,
+         0,47.0,0,
+         0,0,2;
+    system.setMomentOfInertia(convertMatrix<dynamicgraph::Matrix>(I));
+    stateObservation::Matrix4 contactPos1, contactPos2;
+    contactPos1 << 1,0,0,0.095,
+                   0,1,0,0.15,
+                   0,0,1,0,
+                   0,0,0,1;
+    contactPos2 << 1,0,0,0.095,
+                   0,1,0,-0.15,
+                   0,0,1,0,
+                   0,0,0,1;
+    system.setContactPosition(0,contactPos1);
+    system.setContactPosition(1,contactPos2);
+    dynamicgraph::Vector x0; x0.resize(stateSize); x0.setZero();
+    system.setState(x0);
 
-    /// Definitions of input vectors
-
-    /// Definition of ouptut vectors
-    IndexedMatrixArray x;
-    IndexedMatrixArray u;
-
+    /// Stabilizer initialization
     HRP2SMCStabilizer stabilizer("Stabilizer");
+
+
+    IndexedMatrixArray u;
+    IndexedMatrixArray x;
+
+
 
     return 0;
 }
