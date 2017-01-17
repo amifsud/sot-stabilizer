1ddeaa9921f9ad1050154a5d7fa440c9b0a39ab6
Alexis MIFSUD <alexis.mifsud@gmail.com>
2017-01-17 16:47:59 +0100

    complete controller test, before debugging

--- unit-testing/test_hrp2-smc-stabilizer.cpp
+++ unit-testing/test_hrp2-smc-stabilizer.cpp
@@ -2,7 +2,7 @@
 #include <sot-state-observation/tools/definitions.hh>
 #include <state-observation/tools/miscellaneous-algorithms.hpp>
 
-#include <sot-stabilizer/hrp2-smc-stabilizer.hh>
+#include <sot-stabilizer/controllers/discrete-time-ordinary-smc.hh>
 #include <sot-stabilizer/prototyping/non-linear-rotational-table-cart-device.hh>
 
 using namespace sotStabilizer;
@@ -11,18 +11,19 @@ using namespace stateObservation;
 
 int testModel()
 {
-  /// sampling period
+    /// sampling period
     const double dt=5e-3;
 
-  /// Initializations
+    /// Initializations
+
     // Dimensions
     const unsigned kinit=0;
     const unsigned kmax=1400;
 
-    const unsigned stateSize=6;
-    const unsigned controlSize=3;
+    const unsigned stateSize=9;
+    const unsigned controlSize=6;
 
-    /// System initialization
+    // System initialization
     NonLinearRotationalTableCartDevice system("System");
     system.setRobotMass(56.8);
     system.setContactsNumber(2);
@@ -36,34 +37,59 @@ int testModel()
          0,0,2;
     system.setMomentOfInertia(convertMatrix<dynamicgraph::Matrix>(I));
     stateObservation::Matrix4 contactPos1, contactPos2;
-    contactPos1 << 1,0,0,0.095,
+    contactPos1 << 1,0,0,0,
                    0,1,0,0.15,
                    0,0,1,0,
                    0,0,0,1;
-    contactPos2 << 1,0,0,0.095,
+    contactPos2 << 1,0,0,0,
                    0,1,0,-0.15,
                    0,0,1,0,
                    0,0,0,1;
     system.setContactPosition(0,contactPos1);
     system.setContactPosition(1,contactPos2);
-    dynamicgraph::Vector x0; x0.resize(stateSize); x0.setZero();
-    system.setState(x0);
+    stateObservation::Vector x0; x0.resize(2*stateSize+6); x0.setZero();
+    x0.segment(0,3) << 0,
+                       0,
+                       0.8;
+    system.setState(convertVector<dynamicgraph::Vector>(x0));
 
-    /// Stabilizer initialization
-    HRP2SMCStabilizer stabilizer("Stabilizer");
+    // Stabilizer initialization
+    controller::DiscreteTimeOrdinarySMC stabilizer(stateSize, controlSize);
+    stateObservation::Vector x; x.resize(stateSize);
+    stateObservation::Vector dx; dx.resize(stateSize);
+    stateObservation::Vector u; u.resize(stateSize); u.setZero();
+    IndexedMatrixArray state, control;
 
+    // State reference
+    stateObservation::Vector xRef; xRef.resize(stateSize);
+    xRef.setZero();
 
-    IndexedMatrixArray u;
-    IndexedMatrixArray x;
+    stabilizer.setStateDerivativeRef(stateObservation::Vector::Zero(stateSize));
 
+    for(unsigned k=kinit; k<kmax; k++)
+    {
+        // Integrate system
+        system.computeDynamics(dt,u);
+        x = system.getState();
 
+        // Compute next control
+        stabilizer.setState(x.segment(0,stateSize));
+        stabilizer.setStateDerivative(x.segment(stateSize+3,stateSize));
+        stabilizer.setStateRef(xRef);
+        u = stabilizer.getControl(k);
+
+        // Save state
+        state.setValue(x,k);
+        control.setValue(u,k);
+    }
+
+    state.writeInFile("state.dat");
+    control.writeInFile("control.dat");
 
     return 0;
 }
 
 int main()
 {
-
     return testModel();
-
 }
